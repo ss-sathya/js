ffdbb16afd016cabd1d86f44d903bfaa82068e2e
sathya-ss <sathya.narayana@softsuave.org>
2026-02-17 12:43:19 +0530

    Add currying implementation with sum function

---
+++ 17.02.2026.js
@@ -0,0 +1,14 @@
+//function currying
+function sum(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        }
+    }
+}
+
+console.log(sum(1)(2)(3));
+
+const add5 = sum(5);
+
+console.log(add5(10)(15));
