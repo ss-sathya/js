32f0855d9c4230bc59df19da2e69a9874a0aa982
sathya-neon <sathya@neon-ai.uk>
2026-02-19 20:00:08 +0530

    debounce

--- 19.02.2026.js
+++ 19.02.2026.js
@@ -1,7 +1,54 @@
-let array = [];
-array[4] = "hi";
-array.forEach(i => console.log(i));
+// let array = [];
+// array[4] = "hi";
+// array.forEach(i => console.log(i));
 
-setTimeout((a, b) => {
-    console.log(a + b);
-}, 0, 12, 23, 7)
+// setTimeout((a, b) => {
+//     console.log(a + b);
+// }, 0, 12, 23, 7)
+
+
+// const pure = function (value) {
+//     return value + 200;
+// }
+// console.log(pure(40));
+
+// let count = 0;
+// function incrementCount() {
+//     count++;
+// }
+// incrementCount();
+// incrementCount();
+
+// const impure = function () {
+//     return Math.floor(Math.random() * 10);
+// }
+
+// console.log(impure())
+
+function greet() {
+    console.log("Hey Sathya");
+}
+
+greet();
+console.log(greet())
+
+function debounce(fn, delay) {
+    let timer;
+
+    return function (...args) {
+        clearTimeout(timer);
+
+        setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    }
+}
+
+
+const handleSearch = debounce(() => {
+    console.log("API Call");
+}, 50);
+
+const input = document.getElementById("search");
+
+input.addEventListner('keyup', handleSearch)
